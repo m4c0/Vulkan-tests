857c3401d7224f643cd14777c568febc43bf000b
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-23 23:53:19 +0100

    stub for testing multiple outputs

---
+++ multi-output.cpp
@@ -0,0 +1,19 @@
+#pragma leco app
+#pragma leco add_shader "multi-output.frag"
+#pragma leco add_shader "multi-output.vert"
+
+import casein;
+import vee;
+import voo;
+
+struct thread : public voo::casein_thread {
+  void run() override {
+    main_loop("test", [&](auto & dq, auto & sw) {
+      auto pl = vee::create_pipeline_layout();
+      voo::one_quad_render oqr { "multi-output", &dq, *pl };
+
+      ots_loop(dq, sw, [&](auto cb) {
+      });
+    });
+  }
+} i;
