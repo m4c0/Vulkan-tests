3a39afe47d04ea329143ae82b769359027562203
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-24 12:30:40 +0100

    mo: defining a custom render pass

--- multi-output.cpp
+++ multi-output.cpp
@@ -8,13 +8,19 @@ import voo;
 
 struct thread : public voo::casein_thread {
   void run() override {
-    main_loop("test", [&](auto & dq, auto & sw) {
+    voo::device_and_queue dq { "test" };
+    while (!interrupted()) {
+      voo::swapchain_and_stuff sw { dq };
+
+      // TODO: add more attachments
+      auto rp = vee::create_render_pass(dq.physical_device(), dq.surface());
+
       auto pl = vee::create_pipeline_layout();
-      voo::one_quad_render oqr { "multi-output", &dq, *pl };
+      voo::one_quad_render oqr { "multi-output", dq.physical_device(), *rp, *pl };
 
       ots_loop(dq, sw, [&](auto cb) {
         oqr.run(cb, sw.extent());
       });
-    });
+    }
   }
 } i;
